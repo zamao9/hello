56fa038af697004f8963e9fb9fb6a683e5786dc4
zamao9 <94758143+zamao9@users.noreply.github.com>
2022-01-02 17:01:13 +0300

    safsd

--- js/hello.js
+++ js/hello.js
@@ -16,4 +16,23 @@ $(function() {
 
 
 
+                          /* SCROLL */
+
+  $("[data-scroll]").click(function(event) {
+    event.preventDefault();
+
+    var $this = $(this),
+        blockId = $this.data('scroll'),
+        blockOffset = $(blockId).offset().top;
+
+    $("html, body").animate ({
+      scrollTop:blockOffset
+    }, 900);
+
+  })
+
+
+
+
+
 })
